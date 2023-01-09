d8db8bfd309975f61c1cdfd87b17173d57b7a40e
TanmayAgarwal123 <tanmay.agarwal2021@vitstudent.ac.in>
2023-01-09 12:48:07 +0530

    geeksforgeeks

---
+++ Subarray with 0 sum.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+// A C++ program to find if there is a zero sum
+// subarray
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Complete this function
+    //Function to check whether there is a subarray present with 0-sum or not.
+    bool subArrayExists(int arr[], int n)
+    {
+        unordered_map<int,int> ump;
+        int sm=0;
+        ump[0]=1;
+        bool ans=false;
+        for(int i=0;i<n;i++)
+        {
+            sm=sm+arr[i];
+            if(ump.find(sm)!=ump.end())
+            {
+                ans=true;
+                break;
+            }
+            ump[sm]=1;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    Solution obj;
+	    	if (obj.subArrayExists(arr, n))
+		cout << "Yes\n";
+	else
+		cout << "No\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
