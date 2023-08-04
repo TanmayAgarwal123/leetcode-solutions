0bf267228b0ad7fe1834ec025d796d0ae22021eb
TanmayAgarwal123 <tanmay.agarwal2021@vitstudent.ac.in>
2023-08-04 23:30:55 +0530

    change

--- 628. Maximum Product of Three Numbers.cpp
+++ 628. Maximum Product of Three Numbers.cpp
@@ -1,41 +1,10 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        int n =nums.size();
-        int mx[3]={-1001,-1001,-1001};
-        int mn[2]={1001,1001};
-        int fir,sec;
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]>=mx[0])
-            {
-                fir=mx[0];
-                sec=mx[1];
-                mx[0]=nums[i];
-                mx[1]=fir;
-                mx[2]=sec;
-            }
-            else if(nums[i]>=mx[1])
-            {
-                fir=mx[1];
-                mx[1]=nums[i];
-                mx[2]=fir;
-            }
-            else if(nums[i]>mx[2])
-            {
-                mx[2]=nums[i];
-            }
-            if(nums[i]<=mn[0])
-            {
-                fir=mn[0];
-                mn[0]=nums[i];
-                mn[1]=fir;
-            }
-            else if(nums[i]<mn[1])
-            {
-                mn[1]=nums[i];
-            }
-        }
-        return max(mn[0]*mn[1]*mx[0],mx[0]*mx[1]*mx[2]);
+        int prod1, prod2;
+        sort(nums.begin(), nums.end());
+        prod1 = nums[nums.size()-1]*nums[nums.size()-2]*nums[nums.size()-3];
+        prod2 = nums[nums.size()-1]*nums[0]*nums[1];
+        return max(prod1, prod2);
     }
 };
